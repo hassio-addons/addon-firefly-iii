628c45e15769e0e01654f08d475b12cd1e36cf5f
Paul Sinclair <sinclairpaul@hotmail.com>
2020-03-01 11:52:52 -0500

    ✨Use Mariadb services from MariaDB addon

--- firefly-iii/rootfs/etc/cont-init.d/firefly.sh
+++ firefly-iii/rootfs/etc/cont-init.d/firefly.sh
@@ -4,23 +4,21 @@
 # This file creates the file structure and configures the app key
 # ==============================================================================
 
+declare host
+declare key
+declare password
+declare port
+declare username
+
 if ! bashio::fs.directory_exists "/data/firefly/upload"; then
     bashio::log "Creating upload directory"
     mkdir -p /data/firefly/upload
     chown www-data:www-data /data/firefly/upload
 fi
-if ! bashio::fs.directory_exists "/data/firefly/database"; then
-    bashio::log "Creating database directory"
-    mkdir -p /data/firefly/database
-	touch /data/firefly/database/database.sqlite
-    chown -R www-data:www-data /data/firefly/database
-fi
+
 rm -r /var/www/firefly/storage/upload
 ln -s /data/firefly/upload /var/www/firefly/storage/upload
-rm -r /var/www/firefly/storage/database
-ln -s /data/firefly/database /var/www/firefly/storage/database
 
-declare key
 #Create API key if needed
 if ! bashio::fs.file_exists "/data/firefly/appkey.txt"; then
 	#Command fails without appkey set, this won't be used again
@@ -30,25 +28,47 @@ if ! bashio::fs.file_exists "/data/firefly/appkey.txt"; then
  	echo "${key}" > /data/firefly/appkey.txt
  	bashio::log.info "App Key generated: ${key}"
 fi
-if bashio::config.equals 'database' 'mysql';then
-	if ! bashio::config.exists 'mysql_host';then
-		bashio::log.fatal \
-		"MYSQL database has been specified but no host is configured"
-		bashio::exit.nok
-	fi
-	if ! bashio::config.has_value 'mysql_database';then
-		bashio::log.fatal \
-		"MYSQL database has been specified but no database is configured"
-		bashio::exit.nok
-	fi
-	if ! bashio::config.has_value 'mysql_user';then
-		bashio::log.fatal \
-		"MYSQL database has been specified but no user is configured"
-		bashio::exit.nok
-	fi
-	if ! bashio::config.has_value 'mysql_password';then
-		bashio::log.fatal \
-		"MYSQL database has been specified but no password is configured"
-		bashio::exit.nok
-	fi
+
+if bashio::config.has_value 'remote_mysql_host'; then
+  if ! bashio::config.has_value 'remote_mysql_database'; then
+    bashio::exit.nok \
+      "Remote database has been specified but no database is configured"
+  fi
+
+  if ! bashio::config.has_value 'remote_mysql_username'; then
+    bashio::exit.nok \
+      "Remote database has been specified but no username is configured"
+  fi
+
+  if ! bashio::config.has_value 'remote_mysql_password'; then
+    bashio::log.fatal \
+      "Remote database has been specified but no password is configured"
+  fi
+
+  if ! bashio::config.exists 'remote_mysql_port'; then
+    bashio::exit.nok \
+      "Remote database has been specified but no port is configured"
+  fi
+else
+  if ! bashio::services.available 'mysql'; then
+     bashio::log.fatal \
+       "Local database access should be provided by the MariaDB addon"
+     bashio::exit.nok \
+       "Please ensure it is installed and started"
+  fi
+
+  host=$(bashio::services "mysql" "host")
+  password=$(bashio::services "mysql" "password")
+  port=$(bashio::services "mysql" "port")
+  username=$(bashio::services "mysql" "username")
+
+  bashio::log.warning "Firefly-iii is using the Maria DB addon"
+  bashio::log.warning "Please ensure this is included in your backups"
+  bashio::log.warning "Uninstalling the MariaDB addon will remove any data"
+
+  bashio::log.info "Creating database for Firefly-iii if required"
+  mysql \
+    -u "${username}" -p"${password}" \
+    -h "${host}" -P "${port}" \
+    -e "CREATE DATABASE IF NOT EXISTS \`firefly\` ;"
 fi
